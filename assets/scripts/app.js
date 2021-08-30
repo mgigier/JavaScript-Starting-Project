11668c3c3060e3bd3cdaf519e5b4f07f7d72f635
Marcelo Gigier <mo.giggier@usp.br>
2021-08-30 17:56:47 -0300

    Starting Project Ii

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,5 @@ let currentResult = defaultResult;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-let errorMessage = 'An error\n' + 'occured!';
 
-outputResult(currentResult, errorMessage);
+outputResult(currentResult, calculationDescription);
